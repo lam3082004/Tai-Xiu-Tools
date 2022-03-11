86a029a1b95e734f947b8d60bfc29b4d40f0491e
lam3082004 <lam3082004@gmail.com>
2022-03-11 16:50:48 +0700

    11/03/2022

--- main.py
+++ main.py
@@ -2,20 +2,18 @@ from PIL import Image
 import pyautogui as pg
 import pyautogui
 import time
-import random
+import os
 
-TAI_X = 583
-TAI_Y = 664
-XIU_X = 1094
-XIU_Y = 660
-MOT_NGHIN_X = 476
-MOT_NGHIN_Y = 836
-SUBMIT_X = 828
-SUBMIT_Y = 927
-CANCEL_X = 1049
-CANCEL_Y = 929
-KQUA_X = 1093
-KQUA_Y = 774
+TAI_X = 582
+TAI_Y = 685
+XIU_X = 1074
+XIU_Y = 679
+MOT_NGHIN_X = 483
+MOT_NGHIN_Y = 859
+SUBMIT_X = 832
+SUBMIT_Y = 943
+KQUA_X = 1064
+KQUA_Y = 770
 
 MONEY = 5  # 5000 nghin moi lan cuoc
 TIME = 60  # 60s cuoc 1 lan
@@ -23,35 +21,35 @@ TIME = 60  # 60s cuoc 1 lan
 
 def TAI(money):
     pyautogui.click(TAI_X, TAI_Y)
-    time.sleep(1)
+    # time.sleep(1)
     for i in range(money):
         pyautogui.click(MOT_NGHIN_X, MOT_NGHIN_Y)
-    time.sleep(1)
+    # time.sleep(1)
     pyautogui.click(SUBMIT_X, SUBMIT_Y)
 
 
 def XIU(money):
     pyautogui.click(XIU_X, XIU_Y)
-    time.sleep(1)
+    # time.sleep(1)
     for i in range(money):
         pyautogui.click(MOT_NGHIN_X, MOT_NGHIN_Y)
-    time.sleep(1)
+    # time.sleep(1)
     pyautogui.click(SUBMIT_X, SUBMIT_Y)
 
 
-def START():
-    while True:
-        time.sleep(TIME)
-        TAI_XIU_RANDOM = random.randint(1, 100)
-        ps = pg.position()
-        print(ps)
-        print(TAI_XIU_RANDOM)
-        if(TAI_XIU_RANDOM % 2 == 0):
-            TAI(MONEY)
-            print('tai an cut 5k')
-        else:
-            print('xiu an cut 5k')
-            XIU(MONEY)
+# def START():
+#     while True:
+#         time.sleep(TIME)
+#         TAI_XIU_RANDOM = random.randint(1, 100)
+#         ps = pg.position()
+#         print(ps)
+#         print(TAI_XIU_RANDOM)
+#         if(TAI_XIU_RANDOM % 2 == 0):
+#             TAI(MONEY)
+#             print('tai an cut 5k')
+#         else:
+#             print('xiu an cut 5k')
+#             XIU(MONEY)
 
 
 def FIND_TAI_XIU_SUBMIT_POSITION():
@@ -62,35 +60,73 @@ def FIND_TAI_XIU_SUBMIT_POSITION():
 
 
 # SreenShot
-myScreenshot = pyautogui.screenshot()
-myScreenshot.save(r'E:\Tool\Tai-Xiu\Tai-Xiu.png')
+# myScreenshot = pyautogui.screenshot()
+# myScreenshot.save(r'E:\Tool\Tai-Xiu\Tai-Xiu.png')
 
 
-def rgb_of_pixel(img_path, x, y):
-    im = Image.open(img_path).convert('RGB')
-    r, g, b = im.getpixel((x, y))
-    a = (r, g, b)
-    return a
-
-
-dem = 5
-check = True
-img = "Tai-Xiu.png"
 # print(rgb_of_pixel(img, KQUA_X, KQUA_Y))
 
-if((81, 77, 134) == rgb_of_pixel(img, KQUA_X, KQUA_Y)):
-    dem = dem+1
-    if check == False:
-        dem = 0
-    check = True
-else:
-    dem = dem+1
-    if check == True:
-        dem = 0
-    check = False
 
-if(dem >= 5):
-    START()
-else:
-    time.sleep(TIME)
+def START():
+    dem = 0
+    check = -1
+    pick = -1
+    while True:
+
+        myScreenshot = pyautogui.screenshot()
+        myScreenshot.save(r'E:\Tool\Tai-Xiu\Tai-Xiu.png')
+
+        def rgb_of_pixel(img_path, x, y):
+            im = Image.open(img_path).convert('RGB')
+            r, g, b = im.getpixel((x, y))
+            a = (r, g, b)
+            return a
+
+        img = "Tai-Xiu.png"
+
+        # print(rgb_of_pixel(img, KQUA_X, KQUA_Y)[0])
+        # print(rgb_of_pixel(img, KQUA_X, KQUA_Y)[1])
+        # print(rgb_of_pixel(img, KQUA_X, KQUA_Y)[2])
+        print(rgb_of_pixel(img, KQUA_X, KQUA_Y))
+        # time.sleep(30)
+
+        if rgb_of_pixel(img, KQUA_X, KQUA_Y)[0] == 206 and rgb_of_pixel(img, KQUA_X, KQUA_Y)[1] == 255 and rgb_of_pixel(img, KQUA_X, KQUA_Y)[2] == 255:
+            dem = dem+1
+            if check == 1:
+                dem = 1
+            check = 0
+        else:
+            dem = dem+1
+            if check == 0:
+                dem = 1
+            check = 1
+        if check == 0 and dem >= 1:
+            print(dem, 'TÀI')
+        else:
+            print(dem, 'XỈU')
+
+        if check == 0 and pick == 0 or check == 1 and pick == 1 and dem >= 5:
+            dem = 1
+            print('Húp Trọn!!!')
+        time.sleep(30)
+
+        file_path = './Tai-Xiu.png'
+        os.remove(file_path)
+
+        if(dem >= 5):
+            if check == 1:
+                TAI(pow(2, dem-5) * MONEY)
+                print('Đã chọn TÀI')
+                pick = 0
+            else:
+                XIU(pow(2, dem-5) * MONEY)
+                print('Đã chọn XỈU')
+                pick = 1
+            if check == 1 and pick == 1 or check == 0 and pick == 0:
+                dem = 1
+
+        time.sleep(35)
+
+
+START()
 # FIND_TAI_XIU_SUBMIT_POSITION()
--- test.py
+++
@@ -1,4 +0,0 @@
-import pyautogui
-
-myScreenshot = pyautogui.screenshot()
-myScreenshot.save(r'E:\Tool\Tai-Xiu\screenshot_1.png')
